32b43fd7929ec96aa60f5ea7b3b3520fe1b4df6a
Yi <cat2006_03@mail.ru>
2024-09-25 16:08:43 +0300

    without dialogues

--- Scripts/mob_health.gd
+++ Scripts/mob_health.gd
@@ -2,3 +2,13 @@ extends Node2D
 
 @onready var healthBar = $HealthBar
 @onready var damageText = $damageText
+
+func _ready() -> void:
+	pass
+
+func update_heath(new_health,damage):
+	healthBar.value = new_health
+
+func start_parameters(max_health):
+	healthBar.max_value = max_health
+	healthBar.value = max_health
